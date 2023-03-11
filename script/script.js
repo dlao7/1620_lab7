d7a98386cdd6589cf031433f6fc02430cd902f6a
Deborah Lao <dlao7@my.bcit.ca>
2023-03-10 20:23:56 -0800

    Adds constant array courseList to script.js, with 3 course items.

---
+++ script/script.js
@@ -0,0 +1,13 @@
+const courseList = [{
+    code : "ACIT 1620",
+    name : "Fundamental Web Technologies"
+}, 
+{ 
+    code : "ORGB 1100",
+    name : "Organizational Behaviour"
+},
+{
+    code : "COMM 1116",
+    name : "Business Communications 1"
+}
+]
